66be986f403bb54bf3bb9c8e96bcbf8a77f91170
visualstudio <visualstudio@230-PC44>
2019-02-07 12:02:02 -0500

    final commit

--- C0744421_week5/js/main.js
+++ C0744421_week5/js/main.js
@@ -1,1 +1,20 @@
 ﻿// Your code here!
+function sum(a, b) {
+   // var a = 5;
+   // var b = 6;
+    return a + b;
+}
+function arrays() {
+    var pen;
+    pen = ["red", "blue", "green", "orange", "purple"];
+    console.log(pen);
+   // console.log(pen[3]);
+
+    for (i = 0; i < pen.length; i++) {
+        alert(pen[i]);
+
+    }
+    alert("length of array is" + i);
+    alert("length of array is" + pen.length);
+}
+   
